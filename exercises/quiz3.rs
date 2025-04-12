fddb493c4ed9a7b38514221a208157f7452dd544
scbz-lm <learning@mail.scbz.cn.eu.org>
2025-04-12 10:48:59 +0000

    traits3,4,5 test, quiz3, iterators

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,10 +16,9 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub struct ReportCard {
     pub grade: f32,
+    pub grade_str: String,
     pub student_name: String,
     pub student_age: u8,
 }
@@ -28,6 +27,10 @@ impl ReportCard {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
+    }
+    pub fn print_str(&self) -> String {
+        format!("{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade_str)
     }
 }
 
@@ -39,6 +42,7 @@ mod tests {
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
             grade: 2.1,
+            grade_str: "B+".to_string(),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -52,12 +56,13 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: 5.5,
+            grade_str: "A+".to_string(),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
         assert_eq!(
-            report_card.print(),
+            report_card.print_str(),
             "Gary Plotter (11) - achieved a grade of A+"
         );
     }
